a3c71cae6f12a1dc653e93e2ce5bd39ef1300db2
jeillouz <je.illouz9@gmail.com>
2026-04-26 11:09:09 +0200

    Ajout de graphiques

--- analysis.R
+++ analysis.R
@@ -4,8 +4,11 @@ participants = read.csv("csv/participants.csv")
 results = read.csv("csv/results.csv")
 
 library(tidyverse)
+library(tidyselect)
+library(lmerTest)
 
-#Nettoyage des données
+###Formation des données pour l'analyse 
+#Nettoyage et rassemblement des données
 data_clean = results %>%
   #convertir "estCorrect" et "illustration" en vrais booléens 
   mutate(
@@ -23,22 +26,46 @@ data_score = data_clean %>%
     .groups = "drop"
   )
 
+#Calcul de la certitude moyenne par participant pour chaque condition 
 data_certitude = results |>
   mutate(certitude = certitude) |> 
   left_join(participants, by = "id") |>
   group_by(id, illustration, etudiant) |>
   summarise(moy_certitude = mean(certitude, na.rm = TRUE), .groups = "drop")
 
+#Calcul du temps moyen mis par question par participant pour chaque condition 
 data_temps <- results |>
   mutate(tempsSecondes = tempsSecondes) |>
   left_join(participants, by = "id") |>
   group_by(id, illustration, etudiant) |>
   summarise(moy_temps = mean(tempsSecondes, na.rm = TRUE), .groups = "drop")
 
+
+###Données sur les participants 
+#Age
+age_moy = mean(participants$age)
+
+age_moy_etu = mean(as.numeric(participants$age[participants$etudiant == "true"]))
+et_etu = sd(as.numeric(participants$age[participants$etudiant == "true"]))
+
+age_moy_nnetu = mean(as.numeric(participants$age[participants$etudiant == "false"]))
+et_nnetu = sd(as.numeric(participants$age[participants$etudiant == "false"]))
+
+
+boxplot(participants$age ~participants$etudiant, main="Répartition de l'âge selon le statut étudiant", xlab="Statut étudiant", ylab="Age")
+
+#Genre 
+table(participants$genre) #5 femmes et 11 hommes 
+
+
+
 ###Test hypothèse 1 : les performances d'apprentissage seront meilleures pour les parties avec des images 
 
 anova_H1 = aov(data_score$score ~ data_score$illustration)
 summary(anova_H1)
+
+modele_mixte = lmer(score ~ illustration * etudiant + (1|id), data = data_score)
+anova(modele_mixte)
 
 modele_certitude = lmer(moy_certitude ~ illustration * etudiant + (1|id), data = data_certitude)
 anova(modele_certitude)
@@ -46,10 +73,12 @@ anova(modele_certitude)
 modele_temps = lmer(moy_temps ~ illustration * etudiant + (1|id), data = data_temps)
 anova(modele_temps)
 
+#Tests de corrélation 
 chisq.test(data_score$score, data_score$illustration)
 chisq.test(data_clean$illustration, data_clean$tempsSecondes)
 chisq.test(data_clean$illustration, data_clean$certitude)
 
+#Représentations graphiques 
 boxplot(data_score$score~data_score$illustration, main = "Scores obtenus selon qu'il y ait 
   des illustrations ou non", xlab = "Niveau d'illustration", ylab="Scores obtenus")
 
@@ -65,10 +94,16 @@ summary(anova_H2)
 
 chisq.test(data_score$score, data_score$etudiant)
 
+modele_mixte = lmer(score ~ illustration * etudiant + (1|id), data = data_score)
+anova(modele_mixte)
+
+modele_certitude = lmer(moy_certitude ~ illustration * etudiant + (1|id), data = data_certitude)
+anova(modele_certitude)
+
+modele_temps = lmer(moy_temps ~ illustration * etudiant + (1|id), data = data_temps)
+anova(modele_temps)
 
 ###Test hypothèse 1, 2 et 3 : 
-
-library(lmerTest)
 
 modele_mixte = lmer(score ~ illustration * etudiant + (1|id), data = data_score)
 anova(modele_mixte)
@@ -79,4 +114,70 @@ anova(modele_certitude)
 modele_temps = lmer(moy_temps ~ illustration * etudiant + (1|id), data = data_temps)
 anova(modele_temps)
 
+#Pour le groupe étudiant
+data_etudiants <- data_score %>%
+  filter(etudiant == "true") %>%
+  group_by(illustration) %>%
+  summarise(moyenne_score = mean(score),se = sd(score) / sqrt(n()), .groups = "drop")
 
+data_certitude_etud <- data_certitude %>%
+  filter(etudiant == "true") %>%
+  group_by(illustration) %>%
+  summarise(
+    moyenne = mean(moy_certitude, na.rm = TRUE),
+    se = sd(moy_certitude, na.rm = TRUE) / sqrt(n()),
+    .groups = "drop"
+  )
+
+ggplot(data_etudiants, aes(x = illustration, y = moyenne_score, fill = illustration)) +
+  geom_bar(stat = "identity", width = 0.5, color = "black") +
+  scale_fill_manual(values = c("grey", "skyblue"), labels = c("Texte seul", "Avec illustrations")) +
+  labs(
+    title = "Performance des étudiants selon le support",
+    x = "Type de support",
+    y = "Score moyen",
+    fill = "Condition")
+
+ggplot(data_certitude_etud, aes(x = illustration, y = moyenne, fill = illustration)) +
+  geom_bar(stat = "identity", width = 0.5, color = "black") +
+  scale_fill_manual(values = c("grey", "skyblue"), labels = c("Texte seul", "Avec illustrations")) +
+  labs(
+    title = "Certitude des étudiants selon le support",
+    x = "Type de support",
+    y = "Certitude moyenne", 
+    fill = "Condition"
+  )+ ylim(0,7)
+
+#Pour le groupe non-étudiant
+data_nnetudiants <- data_score %>%
+  filter(etudiant == "false") %>%
+  group_by(illustration) %>%
+  summarise(moyenne_score = mean(score),se = sd(score) / sqrt(n()), .groups = "drop")
+
+data_certitude_nnetud <- data_certitude %>%
+  filter(etudiant == "false") %>%
+  group_by(illustration) %>%
+  summarise(
+    moyenne = mean(moy_certitude, na.rm = TRUE),
+    se = sd(moy_certitude, na.rm = TRUE) / sqrt(n()),
+    .groups = "drop"
+  )
+
+ggplot(data_nnetudiants, aes(x = illustration, y = moyenne_score, fill = illustration)) +
+  geom_bar(stat = "identity", width = 0.5, color = "black") +
+  scale_fill_manual(values = c("gray70", "skyblue"), labels = c("Texte seul", "Avec illustrations")) +
+  labs(
+    title = "Performance des non étudiants selon le support",
+    x = "Type de support",
+    y = "Score moyen",
+    fill = "Condition")
+
+ggplot(data_certitude_nnetud, aes(x = illustration, y = moyenne, fill = illustration)) +
+  geom_bar(stat = "identity", width = 0.5, color = "black") +
+  scale_fill_manual(values = c("grey", "skyblue"), labels = c("Texte seul", "Avec illustrations")) +
+  labs(
+    title = "Certitude des non-étudiants selon le support",
+    x = "Type de support",
+    y = "Certitude moyenne", 
+    fill = "Condition"
+  ) + ylim(0,7)
